4425963e41efb8c711ac362b7cdc568a188f356d
user <alexp@users-MacBook-Pro.local>
2018-10-19 08:00:00 -0400

    chore: add history note 17

--- src/test/java/pageObjectTests/Tests3.java
+++ src/test/java/pageObjectTests/Tests3.java
@@ -45,3 +45,4 @@ public class Tests3 {
 // commit note 2 at 2018-06-15T12:00:00
 // commit note 7 at 2018-08-15T09:00:00
 // commit note 12 at 2018-09-14T18:00:00
+// commit note 17 at 2018-10-19T08:00:00
