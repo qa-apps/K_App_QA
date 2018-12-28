2d209bfe765d8dcfe0eba360629522f898e1a5cf
macbook <40374796+qa-apps@users.noreply.github.com>
2018-12-28 00:23:07 -0500

    update

--- src/test/java/dbTest/GetPlaylists.java
+++ src/test/java/dbTest/GetPlaylists.java
@@ -2,3 +2,20 @@ package dbTest;
 
 import helpers.DbAdapter;
 import org.testng.annotations.Test;
+
+public class GetPlaylists {
+    @Test
+    public void getPlaylistsForUser(){
+        var list = DbAdapter.getPlaylistByUserId(801);
+        for (var pl : list){
+            System.out.println("name = " +pl.getName()+" id= "+pl.getId());
+        }
+    }
+    @Test
+    public void getAllArtists(){
+        var list = DbAdapter.getAllArtists();
+        for (var artist: list){
+            System.out.println(artist.getName());
+        }
+    }
+}
