2921e8f61d6c66ade69d718ca34b92d473c2924e
macbook <40374796+qa-apps@users.noreply.github.com>
2019-01-07 00:25:36 -0500

    update

---
+++ src/test/java/helpers/BrowserFabric.java
@@ -0,0 +1,26 @@
+package helpers;
+
+import enums.BrowserType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class BrowserFabric {
+    public static WebDriver getWebDriver(BrowserType browserType){
+        switch (browserType){
+            case OPERA:
+                return getOperaDriver();
+            case EDGE:
+                return getEdgeDriver();
+            case FIREFOX:
+                return getFirefoxDriver();
+            default:
+                return getChromedriver();
+        }
+    }
