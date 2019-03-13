011627d42140bf0b6da4bbdb8e676420a1bf22ae
macbook <pavel.smoljaninov@yahoo.com>
2019-03-13 01:36:43 -0400

    update

--- src/test/java/helpers/DbAdapter.java
+++ src/test/java/helpers/DbAdapter.java
@@ -121,6 +121,51 @@ public class DbAdapter {
         System.out.println("Goodbye!");
         return artists;
     }//end main
+    public static Playlist getPlaylistById(int playlistId) {
+        Playlist playlist = null;
+        final String USERNAME = "dbuser06";
+        final String PASSWORD = "pa$$06";
+        final String URL = "jdbc:mariadb://104.237.9.33/dbkoel";
+        final String DRIVER = "org.mariadb.jdbc.Driver";
+
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Creating table in given database...");
+            statement = connection.createStatement();
+
+            String sql = "SELECT * from playlists p where id = " + playlistId;
+
+            var result = statement.executeQuery(sql);
+            while (result.next()) {
+                int id = result.getInt("id");
+                int user_id = result.getInt("user_id");
+                String name = result.getString("name");
+
+                playlist = new Playlist(id, user_id, name);
+            }
+
+        } catch (SQLException | ClassNotFoundException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (statement != null) {
+                    connection.close();
+
+
+
+
 
 
 
