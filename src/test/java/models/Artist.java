9874fd863c9b183b25887029b54a433ddb3eaf93
macbook <pavel.smoljaninov@yahoo.com>
2019-07-01 01:49:23 -0400

    update

---
+++ src/test/java/models/Artist.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Artist {
+    private int id;
+    private String name;
+    private String image;
+
+    public Artist(int id, String name, String image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
