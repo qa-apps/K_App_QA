4c49b824b5af9eea04046af7bdf340f8fe82a37c
macbook <pavel.smoljaninov@yahoo.com>
2019-07-03 01:50:01 -0400

    update

---
+++ src/test/java/models/Category.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Category {
+    private String name;
+    private long id;
+
+    public Category(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
