50340edaa6f13494a3440ede5b6b50f30cc83b63
macbook <pavel.smoljaninov@yahoo.com>
2019-09-29 01:56:45 -0400

    update

---
+++ src/test/java/pageObjects/LoginPageLocators.java
@@ -0,0 +1,8 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+
+public class LoginPageLocators {
+    public static final String cssEmailLocator = "[type='email']";
+    public static final By byPassword = By.cssSelector("[type='password']");
+}
