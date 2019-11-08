ff4a54df2f38ffa8d93946316f9e9a278b2d56b0
macbook <pavel.smoljaninov@yahoo.com>
2019-11-08 00:59:29 -0500

    update

---
+++ src/test/java/pageObjectTests/LoginTest.java
@@ -0,0 +1,23 @@
+package pageObjectTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.LoginPage;
+import pageObjects.MainPage;
+
+public class LoginTest extends BaseTest{
+    @Test
+    public void loginTest_CorrectCredentials_successfulLogin(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.login(username, password+"nnn");
+        Assert.assertTrue(mainPage.isOpen());
+    }
+    @Test(enabled = false)
+    public void loginTest_wrongCredentials_redFrame(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        loginPage.login(username, wrongPassword);
+        Assert.assertTrue(loginPage.isErrorState());
+    }
+
