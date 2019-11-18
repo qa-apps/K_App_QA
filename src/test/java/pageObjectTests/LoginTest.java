535b1d2896d9710b8f43be808f5952250756007d
macbook <40374796+qa-apps@users.noreply.github.com>
2019-11-18 01:00:13 -0500

    update

--- src/test/java/pageObjectTests/LoginTest.java
+++ src/test/java/pageObjectTests/LoginTest.java
@@ -20,4 +20,34 @@ public class LoginTest extends BaseTest{
         loginPage.login(username, wrongPassword);
         Assert.assertTrue(loginPage.isErrorState());
     }
+    @Test(enabled = false)
+    public void loginTest_CorrectCredentials_successfulLogin1(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.login(username, password);
+        Assert.assertTrue(mainPage.isOpen());
+    }
+    @Test
+    public void loginTest_wrongCredentials_redFrame1(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        loginPage.login(username, wrongPassword);
+        Assert.assertTrue(loginPage.isErrorState());
+    }
+    @Test
+    public void loginTest_CorrectCredentials_successfulLogin2(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.login(username, password);
+        Assert.assertTrue(mainPage.isOpen());
+    }
+    @Test
+    public void loginTest_wrongCredentials_redFrame2(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        loginPage.login(username, wrongPassword);
+        Assert.assertTrue(loginPage.isErrorState());
+    }
+}
+
 
