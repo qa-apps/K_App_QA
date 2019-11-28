f5eb6c1409c32f3b8d9cc71215df8ce5b3943cc1
macbook <pavel.smoljaninov@yahoo.com>
2019-11-28 01:00:48 -0500

    update

---
+++ src/test/java/pageObjectTests/LoginTestA.java
@@ -0,0 +1,30 @@
+package pageObjectTests;
+
+import listeners.RetryAnalyzer;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.LoginPage;
+import pageObjects.MainPage;
+
+public class LoginTestA extends BaseTest{
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void loginTest_CorrectCredentials_successfulLoginA(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.login(username, password);
+        Assert.assertTrue(mainPage.isOpen());
+        boolean pass = false;
+        if(error_count==2){
+            pass=true;
+        }
+        error_count++;
+        Assert.assertTrue(pass);
+    }
+    @Test
+    public void loginTest_wrongCredentials_redFrameA(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        loginPage.login(username, wrongPassword);
+        Assert.assertTrue(loginPage.isErrorState());
+
+
