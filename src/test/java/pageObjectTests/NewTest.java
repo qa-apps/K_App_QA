3da3426ad38bf78ffb50fee4b6773df9f4b86692
macbook <40374796+qa-apps@users.noreply.github.com>
2019-12-28 01:02:20 -0500

    update

---
+++ src/test/java/pageObjectTests/NewTest.java
@@ -0,0 +1,11 @@
+package pageObjectTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+    @Test
+    public void doNothing_AssertTrue(){
+        Assert.assertTrue(true);
+    }
+}
