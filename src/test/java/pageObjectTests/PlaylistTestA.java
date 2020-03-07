29e52d84c28ce5e2ebc1cc4befccc82ef3d73e5b
macbook <40374796+qa-apps@users.noreply.github.com>
2020-03-07 01:07:15 -0500

    update

--- src/test/java/pageObjectTests/PlaylistTestA.java
+++ src/test/java/pageObjectTests/PlaylistTestA.java
@@ -37,3 +37,23 @@ public class PlaylistTestA extends BaseTest{
         Assert.assertTrue(mainPage.checkPlaylistExist(playlistId,name));
     }
 
+    @Test
+    public void playlistTests_renamePlaylist_playlistRenamed1A(){
+        String name = TestObjectGenerator.randomString(8);
+        String newName = faker.ancient().god();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.login(username, password);
+        String playlistId = mainPage.createPlaylist(name);
+        mainPage.renamePlaylist(playlistId,newName);
+        Assert.assertTrue(mainPage.checkPlaylistExist(playlistId,newName));
+    }
+    @Test
+    public void playlistTests_createPlaylist_playlistCreated2A(){
+        String name = faker.funnyName().name();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.login(username, password);
+        String playlistId = mainPage.createPlaylist(name);
+        Assert.assertTrue(mainPage.checkPlaylistExist(playlistId,name));
+
